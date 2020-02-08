14ec0313eba4a9337ea74b89e34c15117eb76f9b
3keeksi <46215747+3keeksi@users.noreply.github.com>
2020-02-08 15:28:03 +0100

    Yeet ein paar funktionen mehr

--- app/src/main/java/at/wunderwuzis/spotify_sync/MainActivity.java
+++ app/src/main/java/at/wunderwuzis/spotify_sync/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
-import com.spotify.protocol.client.Subscription;
-import com.spotify.protocol.types.PlayerState;
-import com.spotify.protocol.types.Track;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +22,61 @@ public class MainActivity extends AppCompatActivity {
     private static final String REDIRECT_URI = "spotifysync://callback";
     private SpotifyAppRemote mSpotifyAppRemote;
 
+    private Button btn_submit;
+
+    private Button btn_login;
+    private Button btn_register;
+
+    private TextView tv_username;
+    private EditText et_password;
+
+
+    private Map<String, String> users = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        init();
+
+        //<editor-fold desc="Swap Click">
+        SwapClick sc = new SwapClick();
+        btn_login.setOnClickListener(sc);
+        btn_register.setOnClickListener(sc);
+        //</editor-fold>
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = (String) tv_username.getText();
+                Editable password = et_password.getText();
+                login(username,password);
+            }
+        });
+
+    }
+
+    public boolean login(String username, Editable password) throws Exception{
+        if(users.containsKey(username)) {
+            if( password.equals(users.get(username)) ) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return new Exception();
+    }
+
+    public void init(){
+        btn_submit = findViewById(R.id.btn_submit);
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        tv_username = findViewById(R.id.tv_username);
+        et_password = findViewById(R.id.et_password);
     }
 
     @Override
@@ -71,5 +125,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    private class SwapClick implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            if( v.getId() == btn_login.getId() ) {
+
+            }
+            else {
+
+            }
     }
 }
