2bbec81a98cfd2753c9a628b32fa277a7b2d4a3e
TheCrether <denisimeri6@gmail.com>
2020-04-07 21:13:06 +0200

    created a new project
    added the spotify sdk

---
+++ app/app/src/main/java/at/wunderwuzis/spotifysync/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package at.wunderwuzis.spotifysync.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
